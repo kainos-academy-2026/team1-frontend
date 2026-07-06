69d1e53402002845a192dae867dd90e4fb845313
e-shn <ellie.shevlin@kainos.com>
2026-07-06 12:23:37 +0100

    Add comments in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,21 @@ import express from "express";
 import "dotenv/config";
 import path from "path";
 import nunjucks from "nunjucks";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Set up Nunjucks as the template engine
 nunjucks.configure(path.join(__dirname, "views"), {
     autoescape: true,
     express: app,
 });
 
+// Route for the localhost:3000 endpoint
 app.get("/", (req, res) => {
     res.render("layouts/base.njk", { title: "Home" });
 });
