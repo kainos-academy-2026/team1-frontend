46ec9e59929ea58ebe5e51d4660231b57d1e3ac5
e-shn <ellie.shevlin@kainos.com>
2026-07-06 12:21:46 +0100

    Configure nunjucks in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import "dotenv/config";
+import path from "path";
+import nunjucks from "nunjucks";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+nunjucks.configure(path.join(__dirname, "views"), {
+    autoescape: true,
+    express: app,
+});
+
+app.get("/", (req, res) => {
+    res.render("layouts/base.njk", { title: "Home" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
-
