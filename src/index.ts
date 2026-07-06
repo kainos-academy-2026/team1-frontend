737feeece7ea0f22cb9442be814aa5f1b9ee8bd8
BenjaminFlan <Benjamin.Flanagan@kainos.com>
2026-07-06 14:09:06 +0100

    bug: restore the change in server port

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 4000;
 
 // Set up Nunjucks as the template engine
 nunjucks.configure(path.join(__dirname, "..", "src", "views"), {
