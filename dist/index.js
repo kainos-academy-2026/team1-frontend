252b6ca936df02cc03c13652fcdc0aafe8d6dfd9
BenjaminFlan <Benjamin.Flanagan@kainos.com>
2026-07-06 13:14:53 +0100

    Fix inability to locate base template

--- dist/index.js
+++ dist/index.js
@@ -3,26 +3,21 @@ import "dotenv/config";
 import path from "path";
 import nunjucks from "nunjucks";
 import { fileURLToPath } from "url";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 console.log("Current directory:", __dirname);
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 // Set up Nunjucks as the template engine
 nunjucks.configure(path.join(__dirname, "..", "src", "views"), {
     autoescape: true,
     express: app,
 });
-
 // Route for the localhost:3000 endpoint
 app.get("/", (req, res) => {
     res.render("layouts/base.njk", { title: "Home" });
 });
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}); 
+    console.log(`Server is running on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
