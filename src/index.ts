fbda14e917c0b54eb6ca4714b4391b2f01506608
BenjaminFlan <Benjamin.Flanagan@kainos.com>
2026-07-06 16:39:37 +0100

    add biome installs & added linting for frontend followed linting comments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import "dotenv/config";
-import { app } from "./app.js";
+import 'dotenv/config';
+import type { Request, Response } from 'express';
+import express from 'express';
+import nunjucks from 'nunjucks';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const PORT = Number(process.env.PORT) || 4000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const  PORT = Number(process.env.PORT) || 4000;
+
+// Set up Nunjucks as the template engine
+const viewsPath = path.join(__dirname, 'views');
+
+nunjucks.configure(viewsPath, {
+	autoescape: true,
+	express: app,
+});
+
+// Route for the home page
+app.get('/', (req: Request, res: Response) => {
+	res.render('layouts/base.njk', { title: 'Home' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
