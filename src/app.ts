379aaff5d784cb50f4bd02748494d75811dbd630
Ben Flanagan <Benjaminflanagan2006@gmail.com>
2026-07-07 10:46:12 +0100

    bug: fix lint call (#6)

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,21 @@
-import express from "express";
-import path from "path";
-import nunjucks from "nunjucks";
-import { fileURLToPath } from "url";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import type { Request, Response } from 'express';
+import express from 'express';
+import nunjucks from 'nunjucks';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const app = express();
 
-const viewsPath = path.join(__dirname, "views");
+const viewsPath = path.join(__dirname, 'views');
 
 nunjucks.configure(viewsPath, {
-    autoescape: true,
-    express: app,
+	autoescape: true,
+	express: app,
 });
 
-app.get("/", (req, res) => {
-    res.render("layouts/base.njk", { title: "Home" });
+app.get('/', (req: Request, res: Response) => {
+	res.render('layouts/base.njk', { title: 'Home' });
 });
