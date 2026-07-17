fa3d9a222837625fcadbe11a1f97b18892a46ec9
Caitlin <caitlinoneill05@gmail.com>
2026-07-17 09:36:36 +0100

    US060 - Pagination Frontend (#18)
    * Updated changes
    
    * Updated Pagenation changes

--- src/services/jobRoleService.ts
+++ src/services/jobRoleService.ts
@@ -1,6 +1,11 @@
 import type { JobRole } from '../models/jobRole.js';
+import type { GetJobRolesPageParams } from './getJobRolesPageParams.js';
+import type { GetJobRolesPageResult } from './getJobRolesPageResult.js';
 
 export interface JobRoleService {
 	getJobRoles(authToken?: string): Promise<JobRole[]>;
+	getJobRolesPage(
+		params: GetJobRolesPageParams,
+	): Promise<GetJobRolesPageResult>;
 	getJobRole(jobRoleId: string, authToken: string): Promise<JobRole | null>;
 }
